14c3fab4ab01bf1ff12cca63a15c2f9ee9eaa15d
Thomas Levine <_@thomaslevine.com>
2014-05-17 14:21:54 +0000

    fp stuff

--- commasearch/fp.py
+++ commasearch/fp.py
@@ -1,4 +1,30 @@
 '''
 Convert URLs into file-like objects.
 '''
-urllib.parse.splittype
+from io import StringIO
+import urllib.parse
+
+import requests
+
+def getfp(url):
+    return functions.get(urllib.parse.urlsplit(url).scheme, not_supported)(url)
+
+def not_supported(url):
+    msg = 'The %s:// scheme is not supported.'
+    scheme = urllib.parse.urlsplit(url).scheme
+    raise ValueError(msg % scheme)
+
+def fromfile(url):
+    _, almost_path = urllib.parse.splittype(url)
+    path = almost_path[1:] # Remove a slash
+    return open(path, 'r')
+
+def fromhttp(url):
+    response = requests.get(url)
+    return StringIO(response.text)
+
+functions = {
+    'file': fromfile,
+    'http': fromhttp,
+    'https': fromhttp,
+}
