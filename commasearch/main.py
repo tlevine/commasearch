01e6c36411681e74e6d897e15f4350a589c17622
Thomas Levine <_@thomaslevine.com>
2014-05-18 23:22:15 +0000

    -

--- commasearch/main.py
+++ commasearch/main.py
@@ -15,9 +15,9 @@ DSV_SCHEMES = {'http','https','file'}
 def index(url:str):
     scheme = urlsplit(url).scheme
     if scheme in DSV_SCHEMES:
-        result = dsv.index(url)
+        result = dsv.index(db, url)
     elif scheme in RDBMS_SCHEMES:
-        result = rdbms.index(url)
+        result = rdbms.index(db, url)
     else:
         raise ValueError('The scheme %s:// is not supported.')
     return result
