a05b0ce16dfa84bb7b68dc5d067da508aae954e0
Thomas Levine <_@thomaslevine.com>
2014-05-18 18:40:33 +0000

    mock db

---
+++ commasearch/test/mockdb.py
@@ -0,0 +1,13 @@
+import collections
+
+class _values:
+    def __init__(self):
+        self.values = collections.defaultdict(lambda: {})
+    def __call__(self, index):
+        return self.values[index]
+
+class mockdb:
+    'Create an in-memory dict database that acts like the disk-backed database.'
+    def __init__(self):
+        self.indices = {}
+        self.values = _values()
