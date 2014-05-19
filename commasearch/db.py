b64f4b5968958c48fe0d85b257f960147cb7e349
Thomas Levine <_@thomaslevine.com>
2014-05-19 01:03:29 +0000

    so much easier to debug

--- commasearch/db.py
+++ commasearch/db.py
@@ -1,7 +1,8 @@
 import os
+import re, json
 
 from pickle_warehouse import Warehouse
 
 home = os.path.expanduser('~')
 indices = Warehouse(os.path.join(home, '.,', 'indices'))
-values = lambda index: Warehouse(os.path.join(home, '.,', 'values', str(hash(index))))
+values = lambda index: Warehouse(os.path.join(home, '.,', 'values', re.sub(r'[\/]', '|', json.dumps(index))))
