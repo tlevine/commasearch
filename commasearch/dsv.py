090b91a156a24a3052ac0fc88a63c2ee86061ea4
Thomas Levine <_@thomaslevine.com>
2014-06-01 13:23:44 -0400

    move stuff

--- commasearch/dsv.py
+++ commasearch/dsv.py
@@ -2,6 +2,7 @@
 Why am I not writing this in Haskell!?
 '''
 import csv
+import itertools
 import re
 from urllib.parse import urlsplit
 from io import StringIO
@@ -20,6 +21,35 @@ def index(db, url:str):
             db.errors[url] = True
         else:
             index_csv(db, fp, url)
+
+def search(db, search_url:str):
+    'Search for table once you have indexed it.'
+
+    # Index the file first.
+    if search_url not in db.indices:
+        raise ValueError('The table must be indexed before you can search it. (%s)' % search_url)
+
+    # Then grab column combinations.
+    colnames = getcolnames(fp, 
+    indices = map(tuple, map(sorted, (itertools.chain(*(itertools.combinations(colnames, i) for i in range(1, len(colnames) + 1))))))
+
+    # Then look for things that have high overlap.
+    for i in indices:
+        search_values = db.values(i)[search_url] # oops. this isn't cached, actually
+        overlaps = [(len(search_values.intersection(result_values)), result_url) for (result_url, result_values) in db.values(i).items()]
+        for overlap_count, url in overlaps:
+            yield overlap_count, i, url
+
+
+
+
+
+def get_colnames(fp) -> list:
+    pos = fp.tell()
+    reader = csv.reader(fp, dialect = dialect)
+    result = next(reader)
+    fp.seek(pos)
+    return result
 
 def guess_dialect(fp):
     'Guess the dialect of a CSV file.'
