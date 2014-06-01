cc2b3dbf9505b4ac2b7b9eae9a0e272891244ed8
Thomas Levine <_@thomaslevine.com>
2014-06-01 18:43:17 -0400

    junk

--- commasearch/rdbms.py
+++ commasearch/rdbms.py
@@ -1,4 +1,13 @@
+import itertools
+
 def index(db, url:str):
+    indices = set()
+    colnames = '???'
+    for x in range(len(colnames)):
+        for i in itertools.combinations(colnames, x):
+            if a subset is not already an index
+                'SELECT ((SELECT count(distinct column, column, column) FROM THE_TABLE) - (SELECT count(*) FROM THE_TABLE))'
+                if that is zero, it's unique!
     raise NotImplementedError
 
 def search(db, url:str):
