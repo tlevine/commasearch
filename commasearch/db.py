de544384772e2bdc2cba1d96d6f2b3b786eae4e4
Thomas Levine <_@thomaslevine.com>
2014-06-05 14:52:31 +0000

    deal with long file names

--- commasearch/db.py
+++ commasearch/db.py
@@ -5,5 +5,9 @@ from pickle_warehouse import Warehouse
 
 home = os.path.expanduser('~')
 indices = Warehouse(os.path.join(home, '.,', 'indices'))
-values = lambda index: Warehouse(os.path.join(home, '.,', 'values', re.sub(r'[\/]', '|', json.dumps(index))))
 errors = Warehouse(os.path.join(home, '.,', 'errors'))
+def values(index):
+    columns = json.dumps(index)
+    subdirectory = columns if len(columns) <= 255 else str(hash(index))
+    directory = os.path.join(home, '.,', 'values', re.sub(r'[\/]', '|', subdirectory))
+    return Warehouse(directory)
